d115957fbc0c3b1cd9e75ffc91fdd08a4b5bf4fd
kwdtje <daan@ef2.nl>
2024-09-30 20:59:34 +0200

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
 	image: {
-		domains: ['vitabelle.daandegooijer.nl']
+		domains: ['vitabelle.daandegooijer.nl', 'vitabelle.vercel.app', 'vitabelle.nl']
 	},
 	integrations: [tailwind()]
 })
